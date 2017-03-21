7749c212c0f1674207cfe83d70dcaad71c801a71
AmyGwyllion <amygwyllion@gmail.com>
2017-03-21 22:32:39 +0100

    integrado jugador con joystick

---
+++ JoystickAndPlayers/Player.cpp
@@ -0,0 +1,192 @@
+/* 
+ * File:   Player.cpp
+ * Author: amygwyllion
+ * 
+ * Created on 21 de marzo de 2017, 8:00
+ */
+
+#include "Player.h"
+#define PPM 64.0f               //PIXELS POR METRO
+#define MPP (1.0f/PPM)          //METROS POR PIXEL
+
+#define pSize 65                //Collider Box Size
+
+Player::Player(){}
+
+Player::Player(b2World &world){
+    
+   //Cargamos la textura
+   if (!texture.loadFromFile("resources/sprites.png")){
+       cout << "Failed to load player spritesheet!" << endl;
+   }
+   
+   //Creamos los diferentes frames que va a tener nuestro sprite animado
+   walkingAnimation.setSpriteSheet(texture);
+   walkingAnimation.addFrame(IntRect(3 , 142 , 49-3 , 207-149));
+   walkingAnimation.addFrame(IntRect(52 , 142 , 97-52 , 207-143));
+   walkingAnimation.addFrame(IntRect(151 , 142 , 196-151 , 207-143));
+   walkingAnimation.addFrame(IntRect(199 , 142 , 243-199 , 207-143));
+   walkingAnimation.addFrame(IntRect(293 , 142 , 339-293 , 207-143));
+   walkingAnimation.addFrame(IntRect(149 , 142 , 196-149 , 207-143));
+
+   jumpAnimation.setSpriteSheet(texture);
+   jumpAnimation.addFrame(IntRect(53,219,98-53,70));
+   jumpAnimation.addFrame(IntRect(101,289-70,146-101,70));
+   jumpAnimation.addFrame(IntRect(149,275-70,196-149,70));
+   jumpAnimation.addFrame(IntRect(199,275-70,247-199,70));
+   jumpAnimation.addFrame(IntRect(250,275-70,303-250,70));
+   jumpAnimation.addFrame(IntRect(306,285-70,351-306,70));
+   jumpAnimation.addFrame(IntRect(354,285-70,399-354,70));
+
+   standAnimation.setSpriteSheet(texture);
+   standAnimation.addFrame(IntRect(4 , 0 , 38-4, 67));
+   standAnimation.addFrame(IntRect(41 , 0 , 74-41, 67));
+   standAnimation.addFrame(IntRect(77 , 0 ,110-77, 67));
+
+   duckAnimation.setSpriteSheet(texture);
+   duckAnimation.addFrame(IntRect(3,298-70,51-3,70));       
+
+   currentAnimation = &standAnimation; //Establecemos la animacion por defecto
+   playerSprite = new SpriteAnimated(seconds(0.2), true, false); //Creamos un sprite animado
+   playerSprite->setOrigin(pSize/2, pSize/2);
+   
+   dirMoving = 0;
+   dirLooking = 1;
+   
+   canJump = true;
+   fuerzaSalto = -65;
+   fuerzaMovimiento = 5;
+   velocidadMaxima = 1;
+   
+    //Definimos un cuerpo dinamico (afectando por la gravedad y resto de objetos box2d creados)
+    bodyDef.type = b2_dynamicBody;
+    bodyDef.gravityScale = 0.7;
+    bodyDef.fixedRotation = true;
+    m_pBody = world.CreateBody(&bodyDef);
+    polyShape.SetAsBox(pSize/2 * MPP, pSize/2 * MPP);
+
+    fixtureDef.shape = &polyShape;
+    fixtureDef.friction = 0.2f;
+    fixtureDef.restitution = 0;
+    fixtureDef.density = 0.7f;
+
+    m_pBody->CreateFixture(&fixtureDef);
+}
+
+Player::Player(const Player& orig) {
+}
+
+Player::~Player() {
+}
+
+void Player::setStandAnimation(){
+    currentAnimation = &standAnimation;
+}
+
+void Player::setWalkingAnimation(){
+    currentAnimation = &walkingAnimation;
+}
+
+void Player::setJumpAnimation(){
+    currentAnimation = &jumpAnimation;
+}
+
+void Player::setDuckAnimation(){
+    currentAnimation = &duckAnimation;
+}
+
+void Player::playAnimation(){
+    playerSprite->play(*currentAnimation);
+}
+
+Animation* Player::getCurrentAnimation(){
+    return currentAnimation;
+}
+
+void Player::update(Time frameTime){
+    playerSprite->update(frameTime);
+    b2Vec2 pos = m_pBody->GetPosition();
+    
+    move();
+    
+    playerSprite->setPosition(pos.x * PPM, pos.y * PPM);
+    playAnimation();
+}
+
+SpriteAnimated& Player::getPlayerSprite(){
+    return *playerSprite;
+}
+
+void Player::jump(){
+    //SALTO
+    
+    //Comprobamos si puede saltar
+    if(!canJump && fabs(m_pBody->GetLinearVelocity().y) <= 0.005){
+        canJump = true;
+    }
+    
+    //Si podemos saltar y el botón no estaba presionado, saltamos
+    if(canJump){
+        m_pBody->SetLinearVelocity(b2Vec2(m_pBody->GetLinearVelocity().x,0));
+        m_pBody->ApplyForceToCenter(b2Vec2(0, fuerzaSalto), 1);
+        canJump = false;
+    }
+    
+}
+
+void Player::changeDirection(int newDirMov){
+    //MOVERSE IZDA/DCHA
+    dirMoving = newDirMov;
+    
+    if(dirMoving != 0){
+        if(!isDucking)setWalkingAnimation();
+        if(dirLooking != dirMoving){
+            playerSprite->scale(-1,1);
+            dirLooking = dirMoving;
+        }
+    }
+    else setStandAnimation();
+}
+
+void Player::move(){
+    if(abs((int)m_pBody->GetLinearVelocity().x) < velocidadMaxima && !isDucking){
+        m_pBody->ApplyForceToCenter(b2Vec2(fuerzaMovimiento * dirMoving, 0), true);
+    }
+}
+
+void Player::duck(int new_dir){
+    if(new_dir == 1){
+        isDucking = true;
+        setDuckAnimation();
+    }
+    
+    else if(new_dir == 0){
+        isDucking = false;
+        setStandAnimation();
+        //if(m_pBody->GetLinearVelocity().x > 0.005)setWalkingAnimation();
+    }
+}
+
+void Player::die(){
+    
+}
+
+void Player::mock(){
+    
+}
+
+void Player::shoot(){
+    
+}
+
+void Player::interact(){
+    
+}
+
+void Player::pause(bool paused){
+    
+}
+
+void Player::setPosition(float posX, float posY){
+    m_pBody->SetTransform(b2Vec2(posX*MPP, posY*MPP),0);
+}
---
+++ JoystickAndPlayers/PlayerJoystick.cpp
@@ -0,0 +1,107 @@
+/* 
+ * File:   PlayerJoystick.cpp
+ * Author: daniel
+ * 
+ * Created on 15 de marzo de 2017, 9:45
+ */
+#include "PlayerJoystick.h"
+
+PlayerJoystick::PlayerJoystick(int id, b2World *world) {
+    boton b1; b1.nombre = "Salto";          b1.numBoton = 0; b1.pulsado = false; misBotones.push_back(b1);
+    boton b2; b1.nombre = "Disparo";        b2.numBoton = 2; b2.pulsado = false; misBotones.push_back(b2);
+    boton b3; b1.nombre = "Interactuar";    b3.numBoton = 1; b3.pulsado = false; misBotones.push_back(b3);
+    boton b4; b1.nombre = "Burlarse";       b4.numBoton = 3; b4.pulsado = false; misBotones.push_back(b4);
+    boton b5; b1.nombre = "Pausar";         b5.numBoton = 7; b5.pulsado = false; misBotones.push_back(b5);
+    
+    estadoEjeX = 0;
+    estadoEjeY = 0;
+    
+    this->id = id;
+    player = new Player(*world);
+    player->setPosition(id*50+100, 100);
+    
+    cout<<"He insertado un mando con la id "<<this->id<<endl;
+}
+
+PlayerJoystick::~PlayerJoystick() {
+}
+
+void PlayerJoystick::pressUpdateState(int pressedButton){
+    if(misBotones.at(0).numBoton == pressedButton && !misBotones.at(0).pulsado){
+        misBotones.at(0).pulsado = true;
+        cout<<id<<".- He saltado"<<endl;
+        player->jump();
+    }
+    
+    else if(misBotones.at(1).numBoton == pressedButton && !misBotones.at(1).pulsado){
+        misBotones.at(1).pulsado = true;
+        cout<<id<<".- He disparado"<<endl;
+        player->shoot();
+    }
+    
+    else if(misBotones.at(2).numBoton == pressedButton && !misBotones.at(2).pulsado){
+        misBotones.at(2).pulsado = true;
+        cout<<id<<".- He interactuado"<<endl;
+        player->interact();
+    }
+    
+    else if(misBotones.at(3).numBoton == pressedButton && !misBotones.at(3).pulsado){
+        misBotones.at(3).pulsado = true;
+        cout<<id<<".- Me he burlado"<<endl;
+        player->mock();
+    }
+    
+    else if(misBotones.at(4).numBoton == pressedButton && !misBotones.at(4).pulsado){
+        misBotones.at(4).pulsado = true;
+        cout<<id<<".- He pausado"<<endl;
+        player->pause(true);
+    }
+}
+
+void PlayerJoystick::releaseUpdateState(int releasedButton){
+    if(misBotones.at(0).numBoton == releasedButton){
+        misBotones.at(0).pulsado = false;
+    }
+    
+    else if(misBotones.at(1).numBoton == releasedButton){
+        misBotones.at(1).pulsado = false;
+    }
+    
+    else if(misBotones.at(2).numBoton == releasedButton){
+        misBotones.at(2).pulsado = false;
+    }
+    
+    else if(misBotones.at(3).numBoton == releasedButton){
+        misBotones.at(3).pulsado = false;        
+    }
+    
+    else if(misBotones.at(4).numBoton == releasedButton){
+        misBotones.at(4).pulsado = false;
+    }
+}
+
+void PlayerJoystick::checkAxisX(int ejeX){
+    if(ejeX > 10) ejeX = 1;
+    else if(ejeX < -10) ejeX = -1;
+    else ejeX = 0;
+    
+    //1 = derecha
+    if(ejeX != estadoEjeX){
+        cout<<"Me muevo en el eje X en la dirección "<<ejeX<<endl;
+        estadoEjeX = ejeX;
+        player->changeDirection(estadoEjeX);  
+    }
+}
+
+void PlayerJoystick::checkAxisY(int ejeY){
+    if(ejeY > 10) ejeY = 1;
+    else if(ejeY < -10) ejeY = -1;
+    else ejeY = 0;
+    
+    //1 = agachado
+    if(ejeY != estadoEjeY){
+        cout<<"Me muevo en el eje Y en la dirección "<<ejeY<<endl;
+        estadoEjeY = ejeY;
+        player->duck(estadoEjeY);  
+    }
+}
