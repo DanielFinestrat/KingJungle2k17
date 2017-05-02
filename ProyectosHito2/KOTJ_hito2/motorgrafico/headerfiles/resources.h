a8eec1066fac8dd41b4fa7503714ecf2c27b6aeb
danielfinestrat <danielfinestratmartinez@hotmail.com>
2017-05-02 09:21:20 +0200

    Entrega añadida

---
+++ ProyectosHito2/KOTJ_hito2/motorgrafico/headerfiles/resources.h
@@ -0,0 +1,49 @@
+
+#ifndef __RESOURCES_H__
+#define __RESOURCES_H__
+
+#include <SFML/Graphics.hpp>
+
+using namespace std;
+using namespace sf;
+
+class Resources {
+    
+    public:
+       
+        static Resources* getInstance();
+        bool initialise();
+        void cleanUp();
+        bool loadAll();
+        
+        bool loadFont(string filename);
+        Font &getFont(string filename);
+        
+        bool loadTexture(string filename);
+        Texture &getTexture(string filename);
+        
+        bool loadImage(string filename);
+        Image &getImage(string filename);
+
+        const string myFont;
+        const string triangle;
+        const string sprites;
+        const string armas;
+        const string tiles;
+        const string fondoJungle;        
+        const string fondoHielo;        
+        const string fondoHielo1;        
+        const string fondoSeleccion;        
+        const string fondoCueva;        
+        const string fondoMandala;        
+        const string fondoMar;        
+
+    private:
+        Resources();
+        map<string, Font*> fonts;
+        map<string, Texture*> textures;
+        map<string, Image*> images;
+
+};
+
+#endif
