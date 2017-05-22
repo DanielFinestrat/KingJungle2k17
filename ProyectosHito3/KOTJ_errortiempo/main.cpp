1a68d18954667e1ff56a13c5e10af524bf2cbe58
danielfinestrat <danielfinestratmartinez@hotmail.com>
2017-05-22 15:54:14 +0200

    Error de tiempo

---
+++ ProyectosHito3/KOTJ_errortiempo/main.cpp
@@ -0,0 +1,206 @@
+
+#include "headerfiles/Partida.h"
+#include "motorgrafico/headerfiles/Texto.h"
+#include "motorgrafico/headerfiles/Motorgrafico.h"
+#include "motorgrafico/headerfiles/musicPlayer.h"
+
+using namespace std;
+
+int state;
+
+Menu* createMainMenu() {
+    Texto* op1 = new Texto("Jugar", 40, "./resources/fonts/newrotic.ttf", 255, 0, 0);
+    Texto* op2 = new Texto("Opciones de partida", 40, "./resources/fonts/newrotic.ttf", 255, 0, 0);
+    Texto* op3 = new Texto("Opciones del sonido", 40, "./resources/fonts/newrotic.ttf", 255, 0, 0);
+    Texto* op4 = new Texto("Salir", 40, "./resources/fonts/newrotic.ttf", 255, 0, 0);
+
+    std::vector<Texto*> vec(4);
+    vec[0] = op1;
+    vec[1] = op2;
+    vec[2] = op3;
+    vec[3] = op4;
+
+    Menu* menu = new Menu(vec, 350, 200);
+    return menu;
+}
+
+Menu* createGameMenu(int rondas, int tiempo, int modo) {
+    Texto* op1 = new Texto("Puntos", 40, "./resources/fonts/newrotic.ttf", 255, 0, 0);
+    Texto* op2 = new Texto("Tiempo", 40, "./resources/fonts/newrotic.ttf", 255, 0, 0);
+    Texto* op3 = new Texto("Modo", 40, "./resources/fonts/newrotic.ttf", 255, 0, 0);
+    Texto* op4 = new Texto("Volver", 40, "./resources/fonts/newrotic.ttf", 255, 0, 0);
+
+    std::vector<Texto*> vec(4);
+    vec[0] = op1;
+    vec[1] = op2;
+    vec[2] = op3;
+    vec[3] = op4;
+
+    Menu* menu = new Menu(vec, 350, 200, rondas, tiempo, modo);
+
+    return menu;
+}
+
+Menu* createOptionsMenu() {
+    Texto* op1 = new Texto("Volumen de musica", 40, "./resources/fonts/newrotic.ttf", 255, 0, 0);
+    Texto* op2 = new Texto("Volumen de efectos", 40, "./resources/fonts/newrotic.ttf", 255, 0, 0);
+    Texto* op3 = new Texto("Volver", 40, "./resources/fonts/newrotic.ttf", 255, 0, 0);
+
+    std::vector<Texto*> vec(3);
+    vec[0] = op1;
+    vec[1] = op2;
+    vec[2] = op3;
+
+    int vm = Motorgrafico::getInstance()->getMusicPlayer()->getMusicVolume();
+    int vfx = Motorgrafico::getInstance()->getMusicPlayer()->getSFXVolume();
+    Menu* menu = new Menu(vec, 350, 200, vm, vfx);
+    return menu;
+}
+
+Menu* createPause() {
+    Texto* op1 = new Texto("Reanudar", 40, "./resources/fonts/newrotic.ttf", 255, 0, 0);
+    Texto* op2 = new Texto("Volver al menu", 40, "./resources/fonts/newrotic.ttf", 255, 0, 0);
+    Texto* op3 = new Texto("Salir del juego", 40, "./resources/fonts/newrotic.ttf", 255, 0, 0);
+
+    std::vector<Texto*> vec(3);
+    vec[0] = op1;
+    vec[1] = op2;
+    vec[2] = op3;
+
+    Menu* menu = new Menu(vec, 350, 200);
+    return menu;
+}
+
+int main() {
+    bool empezada = false;
+
+    state = 1;
+    Menu* menu;
+    Motorgrafico *mg = Motorgrafico::getInstance();
+    Fondo* fondo = new Fondo("./resources/fondos/fondoCarga.png", true);
+    fondo->Update();
+    fondo->Render();
+    mg->createMusicPlayer();
+    mg->getMusicPlayer()->playSound(mg->getMusicPlayer()->menuMusic);
+
+    //datos Basicos de la partida
+    vector<int> datosPartida; //int puntos tiempo modo
+    datosPartida.push_back(5);
+    datosPartida.push_back(60);
+    datosPartida.push_back(1);
+
+    while (state != 0) {
+
+        if (state == 1) {
+            if (empezada) {
+                Partida::getInstance()->worldPlayer.clear();
+                Partida::getInstance()->worldControlador.clear();
+                Motorgrafico::getInstance()->setGoToMenu(false);
+                Partida::getInstance()->lastLevel = false;
+                Partida::getInstance()->gameisover = false;
+                Partida::getInstance()->setUsingKeyboard(false);
+            }
+
+            empezada = false;
+            Motorgrafico::getInstance()->setGoToMenu(false);
+
+            menu = createMainMenu();
+            mg->getMusicPlayer()->setLoop(mg->getMusicPlayer()->menuMusic);
+
+            while (mg->getRenderWindow()->isOpen() && state == 1) {
+                menu->input(state, menu);
+                menu->update();
+                menu->render();
+            }
+
+            menu = NULL;
+        }
+
+        if (state == 2) {
+            if (!empezada) {
+                Partida* partida;
+                Motorgrafico::getInstance()->createPartida(datosPartida.at(1));
+                partida = Motorgrafico::getInstance()->getPartida();
+                partida->indexMap = -1;
+                Partida::getInstance()->setMaxPoints(datosPartida.at(0));
+                Partida::getInstance()->fillRondasVector(datosPartida.at(2));
+                partida->loadMap("./resources/mapas/mapaSeleccion.tmx");
+                Partida::getInstance()->checkJoysticksConnected();
+                empezada = true;
+            }
+
+            mg->getMusicPlayer()->stopSound(mg->getMusicPlayer()->menuMusic);
+
+            if (Motorgrafico::getInstance()->getPartida()->mapa->getIfFirstMap()) {
+                mg->getMusicPlayer()->playSound(mg->getMusicPlayer()->elevatorMusic);
+                mg->getMusicPlayer()->setLoop(mg->getMusicPlayer()->elevatorMusic);
+            } else {
+                mg->getMusicPlayer()->playSound(mg->getMusicPlayer()->battleMusic);
+                mg->getMusicPlayer()->setLoop(mg->getMusicPlayer()->battleMusic);
+            }
+
+            while (mg->getRenderWindow()->isOpen() && state == 2) {
+                mg->getPartida()->Input(state);
+                if (mg->getPartida()->getInstance()->loadingLevelStruct.loadingLevel) {
+                    mg->getPartida()->updateBeforeMap();
+                } else if (mg->getPartida()->getInstance()->loadingLevelStruct.showingInbetween) {
+                    mg->getPartida()->inbetweenUpdate();
+                } else {
+                    mg->getPartida()->Update();
+                }
+                mg->getPartida()->Erase();
+                mg->getPartida()->Render();
+
+            }
+
+            mg->getMusicPlayer()->stopSound(mg->getMusicPlayer()->battleMusic);
+            mg->getMusicPlayer()->stopSound(mg->getMusicPlayer()->elevatorMusic);
+
+            Hud::getInstance()->deletePlayers();
+        }
+
+
+        if (state == 3) {
+
+            menu = createGameMenu(datosPartida.at(0), datosPartida.at(1), datosPartida.at(2));
+
+            while (mg->getRenderWindow()->isOpen() && state == 3) {
+                menu->input(state, menu);
+                menu->gameUpdate();
+                menu->renderGameMenu();
+            }
+
+            datosPartida.at(0) = menu->getPuntos();
+            datosPartida.at(1) = menu->getTiempo();
+            datosPartida.at(2) = menu->getMode();
+
+            menu = NULL;
+        }
+
+        if (state == 4) {
+            menu = createOptionsMenu();
+
+            while (mg->getRenderWindow()->isOpen() && state == 4) {
+                menu->input(state, menu);
+                menu->optionsUpdate();
+                menu->renderOptionsMenu();
+            }
+            menu = NULL;
+        }
+
+        if (state == 5) {
+            Menu* pauseMenu = createPause();
+            while (Motorgrafico::getInstance()->getRenderWindow()->isOpen() && state == 5) {
+                pauseMenu->input(state, pauseMenu);
+                pauseMenu->update();
+                pauseMenu->render();
+            }
+            pauseMenu = NULL;
+        }
+    }
+
+    mg = NULL;
+    delete mg;
+    delete menu;
+    return 0;
+}
---
+++ ProyectosHito3/KOTJ_errortiempo/sourcefiles/Coinsspawner.cpp
@@ -0,0 +1,145 @@
+
+/* 
+ * File:   Coinsspawner.cpp
+ * Author: amygwyllion
+ * 
+ * Created on 1 de mayo de 2017, 16:44
+ */
+
+#include "../headerfiles/Coinsspawner.h"
+#include "../headerfiles/Partida.h"
+#include "../motorgrafico/headerfiles/Motorgrafico.h"
+
+Coinsspawner::Coinsspawner() {   
+    leerCoins();
+    leerSpawnerPosition();
+    cargarCoins();
+    
+    currentTime =  respawnTime;
+    
+    clock = new InnerClock();
+    clock->restartClock();
+}
+
+Coinsspawner::Coinsspawner(const Coinsspawner& orig) {
+}
+
+void Coinsspawner::leerSpawnerPosition(){
+    deleteCoins();
+    spawnCoins = Partida::getInstance()->mapa->getSpawnCoins();
+}
+
+void Coinsspawner::leerCoins(){
+    if(!texture.loadFromFile(Resources::getInstance()->coin)){
+        cout<<"Error al cargar textura"<<endl;
+    }
+    
+    const char *cstr = coinsFile.c_str();
+    TiXmlDocument doc(cstr);
+    doc.LoadFile();
+    
+    TiXmlElement* coins = doc.FirstChildElement("coins");
+    TiXmlElement* coin = coins->FirstChildElement("coin");
+    
+    int i = 0;
+    
+    while(coin){
+        datosCoin nuevosDatos;
+        coinAnimation = new Animation();
+        coinAnimation->setSpriteSheet(texture);
+        nuevosDatos.indice = i;
+        
+        coin->QueryIntAttribute("valor", &nuevosDatos.valor);
+        coin->QueryIntAttribute("rareza", &nuevosDatos.rareza);
+        
+        TiXmlElement* sprite = coin->FirstChildElement("sprite");
+        float x, y, w, h;
+        
+        while(sprite){
+            
+            sprite->QueryFloatAttribute("x",&x);
+            sprite->QueryFloatAttribute("y",&y);
+            sprite->QueryFloatAttribute("w",&w);
+            sprite->QueryFloatAttribute("h",&h);
+            
+            //cout<<"para i = "<< i <<" "<< x <<" "<< y <<" "<< w <<" "<< h <<endl;
+            coinAnimation->addFrame(IntRect(x,y,w,h));
+            sprite = sprite->NextSiblingElement("sprite");
+        }
+        nuevosDatos.animation = coinAnimation;
+        datosCoins.push_back(nuevosDatos);
+        coin = coin->NextSiblingElement("coin");
+        i++;
+    }
+}
+
+void Coinsspawner::Update(){
+    currentTime = currentTime - clock->getDeltaTimeAsSeconds();
+    clock->restartClock();
+    
+    if (currentTime <= 0) {
+        currentTime = respawnTime;
+        //checkIfUsed();
+        deleteCoins();
+        cargarCoins();
+    }
+
+}
+
+void Coinsspawner::cargarCoins(){
+    Partida::getInstance()->worldCoins.clear();
+    srand(time(NULL));
+    for (int i = 0; i < spawnCoins.size(); i++) {
+
+       datosCoin datos;
+       int grupo = -1;
+       int indice = rand() % 20; //100% (0 a 19)
+
+       if (indice <= 8) grupo = 0; //40%
+       else if (indice <= 15) grupo = 1; //35%
+       else if (indice <= 19) grupo = 2; //25%
+
+       do {
+           indice = rand() % datosCoins.size();
+           datos = datosCoins.at(indice);
+       } while (grupo != datos.rareza);
+       
+       vector<int> spawn = spawnCoins.at(i);
+       float x = spawn.at(0);
+       float y = spawn.at(1);
+
+       Coin* coin = new Coin(x,y,datos.valor, datos.animation);
+       
+       coins.push_back(coin);
+       Partida::getInstance()->worldCoins.push_back(coin);
+   }
+    cout<<"salgo de cargarCoins con el vector worldCoins de tamaño: "<<Partida::getInstance()->worldCoins.size()<<endl;
+}
+
+void Coinsspawner::checkIfUsed(){
+    
+    for (int i = 0; i < coins.size(); i++) {
+        if (coins.at(i) != NULL) {
+            Coin *coin = coins.at(i);
+            if (coin->getInPossession()) {
+                Partida::getInstance()->coins2Delete.push_back(coins.at(i));
+                Partida::getInstance()->worldCoins.at(i) = NULL;
+                coins.at(i) == NULL;
+            }
+        }
+    }
+}
+
+void Coinsspawner::deleteCoins(){
+    for (int i = 0; i < Partida::getInstance()->worldCoins.size(); i++) {
+        Coin* coin = Partida::getInstance()->worldCoins.at(i);
+        if (coin != NULL) {
+            Partida::getInstance()->coins2Delete.push_back(Partida::getInstance()->worldCoins.at(i));
+            Partida::getInstance()->worldCoins.at(i) = NULL;
+        }
+    }
+    Partida::getInstance()->worldCoins.clear();
+}
+
+Coinsspawner::~Coinsspawner() {
+}
